b2c5f00e8caa7cd10de4cf25c3962133c3968b5c
Stefan Tagarski <116569281+stefantagarski@users.noreply.github.com>
2024-12-04 21:29:22 +0100

    Update Transakcija_shalter.sql

--- labs/lab3A/Transakcija_shalter.sql
+++ labs/lab3A/Transakcija_shalter.sql
@@ -7,7 +7,7 @@ CREATE TABLE Transakcija_shalter (
     datum DATE,
     suma INT,
     tip VARCHAR(10),
-    FOREIGN KEY (MBR_k_s, broj) REFERENCES Smetka(MBR_k, broj) ON DELETE NO ACTION ON UPDATE NO ACTION,
+    FOREIGN KEY (MBR_k_s, broj) REFERENCES Smetka(MBR_k, broj) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT datumTransakcija CHECK (NOT (datum >= '2020-12-30' AND datum <= '2021-01-14')),
     CONSTRAINT timTransakcija CHECK (tip IN ('isplata', 'uplata'))
 );
