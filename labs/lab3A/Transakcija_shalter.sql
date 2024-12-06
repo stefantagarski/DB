c98e417ba8a499fef6f3f83cb9db67ff870a3545
Stefan Tagarski <116569281+stefantagarski@users.noreply.github.com>
2024-12-06 19:33:59 +0100

    Update Transakcija_shalter.sql

--- labs/lab3A/Transakcija_shalter.sql
+++ labs/lab3A/Transakcija_shalter.sql
@@ -1,13 +1,15 @@
-CREATE TABLE Transakcija_shalter (
+-- Transakcija_shalter(ID, ID_v*, MBR_k*, MBR_k_s*, broj*, datum, suma, tip)
+
+CREATE TABLE Transakcija_shalter(
     ID INT PRIMARY KEY,
-    ID_v INT FOREIGN KEY REFERENCES Shalterski_rabotnik(ID) ON DELETE SET NULL ON UPDATE CASCADE,
+    ID_v INT FOREIGN KEY REFERENCES Shalterski_Rabotnik(ID) ON DELETE SET NULL ON UPDATE CASCADE,
     MBR_k INT FOREIGN KEY REFERENCES Klient(MBR_k) ON DELETE CASCADE ON UPDATE CASCADE,
     MBR_k_s INT,
     broj INT,
     datum DATE,
     suma INT,
-    tip VARCHAR(10),
-    FOREIGN KEY (MBR_k_s, broj) REFERENCES Smetka(MBR_k, broj) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT datumTransakcija CHECK (NOT (datum >= '2020-12-30' AND datum <= '2021-01-14')),
-    CONSTRAINT timTransakcija CHECK (tip IN ('isplata', 'uplata'))
+    tip VARCHAR(50),
+    CONSTRAINT TransSHalter_FK FOREIGN KEY (MBR_k_s, broj) REFERENCES Smetka(MBR_k, broj) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT datum_Izvrsuvanje CHECK (NOT (datum >= '2020-12-30' AND datum <= '2021-10-14')),
+    CONSTRAINT tip_Transakcija CHECK (tip IN ('uplata', 'isplata'))
 );
